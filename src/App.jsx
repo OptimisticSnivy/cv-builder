5678ce011fda69388adc938ff4a62efaa4ae5204
Anand Mahamuni <m.anand7203@gmail.com>
2024-02-27 19:23:06 +0530

    Layout Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-
-  return (
-    <>
-    </>
-  )
+function Form({text}) {
+	return (
+		<form className="form">
+			<label>
+				{text}:
+				<input type="text" name="name" />
+			</label>
+			<input type="submit" value="Submit" />
+		</form>
+	);
 }
 
-export default App
+Form.defaultProps = {
+	text: "Name"
+}
+
+function Layout(){
+	return (
+		<div>
+			<h2> Basic </h2>
+			<Form />
+			<Form text="Phone No."/>
+			<Form text="E-Mail"/>
+			<h2> Education </h2>
+			<Form text="College"/>
+			<Form text="Degree"/>
+			<Form text="Term"/>
+		</div>
+	);
+}
+
+export default function App() {
+	return <Layout />
+}
